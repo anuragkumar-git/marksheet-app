57ec16ba354ed68c3105dfd35af9f7cd2f371995
anuragkumar-git <patelanurag3971@gmail.com>
2026-05-21 11:19:00 +0530

    feat: share pdf

--- src/pages/Preview.jsx
+++ src/pages/Preview.jsx
@@ -1,35 +1,60 @@
-import Layout from '../components/Layout'
-import { useMarksheet } from '../context/MarksheetContext'
-import PDFDocument from '../components/PDFDocument'
+import Layout from "../components/Layout";
+import { useMarksheet } from "../context/MarksheetContext";
+import PDFDocument from "../components/PDFDocument";
 
-import {
-  PDFViewer,
-  PDFDownloadLink,
-} from '@react-pdf/renderer'
+import { PDFViewer, PDFDownloadLink, pdf } from "@react-pdf/renderer";
+import { useState } from "react";
 
 function Preview() {
-  const { formData } = useMarksheet()
+  const { formData } = useMarksheet();
+  const [sharing, setSharing] = useState(false);
+  const handleShare = async () => {
+    try {
+      setSharing(true);
+      const blob = await pdf(<PDFDocument data={formData} />).toBlob();
+
+      const fileName = `${formData.studentName || "Student"}Marksheet.pdf`;
+
+      const file = new File([blob], fileName, { type: "application/pdf" });
+      if (navigator.canShare && navigator.canShare({ files: [file] })) {
+        await navigator.share({
+          title: `${fileName}`,
+          text: "Sharing marksheet PDF",
+          files: [file],
+        });
+        setSharing(false);
+      } else {
+        alert("Sharing is not supported on your device.");
+        setSharing(false);
+      }
+    } catch (error) {
+      console.error(error);
+      alert("Failed to share PDF");
+      setSharing(false);
+    }
+  };
 
   return (
-    <Layout title='PDF Preview'>
-      <div className='preview-container'>
-        <PDFViewer width='100%' height='600'>
+    <Layout title="PDF Preview">
+      <div className="preview-container">
+        <PDFViewer width="100%" height="600">
           <PDFDocument data={formData} />
         </PDFViewer>
       </div>
 
-      <div className='download-section'>
+      <div className="download-section">
         <PDFDownloadLink
           document={<PDFDocument data={formData} />}
-          fileName={`${formData.studentName || 'Student'}Marksheet.pdf`}
+          fileName={`${formData.studentName || "Student"}Marksheet.pdf`}
         >
-          {({ loading }) =>
-            loading ? 'Generating PDF...' : 'Download PDF'
-          }
+          {({ loading }) => (loading ? "Generating PDF..." : "Download PDF")}
         </PDFDownloadLink>
+        <button onClick={handleShare} disabled={sharing}>
+          Share PDF
+        </button>
       </div>
     </Layout>
-  )
+  );
 }
 
-export default Preview
+export default Preview;
